94b19d230f86e4ae941e35614bbb043bcc0dd2db
Andrey Grytsenko <andreyapg@gmail.com>
2024-02-05 19:53:21 +0200

    03 iloc 2024-02-05

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import pandas as pd
 
-df = pd.read_csv('data.csv')
+df = pd.read_csv('salaries.csv')
 
-print(df.head())
+#x = df.iloc[0, 1]
+x = df.iloc[:3, :2]
+#print(x)
+
+#y = df.iloc[:, -1]
+y = df.iloc[:, -1].values
+# print(y)
+print(y[0:5])
