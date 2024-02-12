8eedd61d7ed85f0c8f5939dc16a303d62ed4bfb8
Andrey Grytsenko <andreyapg@gmail.com>
2024-02-12 14:41:35 +0200

    Comments 2024-02-12

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Read the data
 df = pd.read_csv('salaries.csv')
 
+# Split the data into x and y
 x = df.iloc[:, :-1].values
 y = df.iloc[:, :-1].values
 
+# Plot the data
 plt.scatter(x, y)
 plt.show()
