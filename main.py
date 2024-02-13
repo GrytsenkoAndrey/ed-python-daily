ae682bfa793fb7c546240c728d09edf17a89d4a7
Andrey Grytsenko <andreyapg@gmail.com>
2024-02-13 17:11:37 +0200

    05 Split into Training and Testing Data (Linear Regression Salaries Course) 2024-02-13

--- main.py
+++ main.py
@@ -11,3 +11,7 @@ y = df.iloc[:, :-1].values
 # Plot the data
 plt.scatter(x, y)
 plt.show()
+
+from sklearn.model_selection import train_test_split
+x_train, x_text, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+print(x_train)
