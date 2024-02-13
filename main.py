7e189ef41572a441c8f294b481ddc98db9208be9
Andrey Grytsenko <andreyapg@gmail.com>
2024-02-13 17:12:06 +0200

    05 use Shape 2024-02-13

--- main.py
+++ main.py
@@ -14,4 +14,4 @@ plt.show()
 
 from sklearn.model_selection import train_test_split
 x_train, x_text, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
-print(x_train)
+print(x_train.shape)
